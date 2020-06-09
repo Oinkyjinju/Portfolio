2e67e5206dac3512bb1db57f5f9861eb0e172bde
Oinkyjinju <oinkyjinju@gmail.com>
2020-06-08 23:38:25 -0600

    track large files

--- fader.js
+++
@@ -1,8 +0,0 @@
-$('#submit').on('click', function() {
-    if ($('#next-fade').css('opacity') == 1) {
-        $('#next-fade').css('opacity', 0);
-    }
-    else {
-        $('#next-fade').css('opacity', 1);
-    }
-});
